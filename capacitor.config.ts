b4742db6fdc867c795d327b056718c097029c03a
OIssa73 <oissa73@example.com>
2026-07-28 01:00:14 +0200

    Revert: retour a la compilation des fichiers locaux pour l'application mobile

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,11 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.axis24.mediahub',
   appName: 'Axis24',
-  webDir: 'dist',
-  server: {
-    url: 'https://axis24media.vercel.app',
-    cleartext: true
-  }
+  webDir: 'dist'
 };
 
 export default config;
